73f1a65ae315d874c89fcff41a67af3ec150285d
nobel <nobel2073@gmail.com>
2025-12-05 17:44:54 +0545

    chore: import stdlib in test programs

--- stdlib/runtime.c
+++ stdlib/runtime.c
@@ -1,123 +1,91 @@
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-double runtime_write(double fd, const char *buf, double count)
+double aloha_sys_write(double fd, const char *buf, double count)
 {
-    int fd_int = (int)fd;
-    size_t count_size = (size_t)count;
-    if (fd_int == 1)
-    {
-        return (double)fwrite(buf, 1, count_size, stdout);
-    }
-    else if (fd_int == 2)
-    {
-        return (double)fwrite(buf, 1, count_size, stderr);
-    }
-    return -1.0;
+    return (double)write((int)fd, buf, (size_t)count);
 }
 
-double runtime_read(double fd, char *buf, double count)
+double aloha_sys_read(double fd, char *buf, double count)
 {
-    int fd_int = (int)fd;
-    size_t count_size = (size_t)count;
-    if (fd_int == 0)
-    {
-        return (double)fread(buf, 1, count_size, stdin);
-    }
-    return -1.0;
+    return (double)read((int)fd, buf, (size_t)count);
 }
 
-void runtime_flush(double fd)
-{
-    int fd_int = (int)fd;
-    if (fd_int == 1)
-    {
-        fflush(stdout);
-    }
-    else if (fd_int == 2)
-    {
-        fflush(stderr);
-    }
-}
-
-void *runtime_malloc(double size)
-{
-    return malloc((size_t)size);
-}
-
-void runtime_free(void *ptr)
-{
-    free(ptr);
-}
-
-double runtime_strlen(const char *str)
+double aloha_sys_strlen(const char *str)
 {
     return (double)strlen(str);
 }
 
-double runtime_memcmp(const char *a, const char *b, double n)
-{
-    return (double)memcmp(a, b, (size_t)n);
-}
-
-void runtime_memcpy(char *dst, const char *src, double n)
-{
-    memcpy(dst, src, (size_t)n);
-}
-
-void runtime_exit(double code)
+void aloha_sys_exit(double code)
 {
     exit((int)code);
 }
 
-void runtime_abort(void)
+double aloha_sys_num_to_str(double value, char *buf, double buf_size)
 {
-    abort();
+    int len = snprintf(buf, (size_t)buf_size, "%g", value);
+    return (double)(len > 0 ? len : 0);
 }
 
-void print(const char *str)
-{
-    runtime_write(1, str, (double)strlen(str));
-}
-
-void println(const char *str)
-{
-    runtime_write(1, str, (double)strlen(str));
-    runtime_write(1, "\n", 1.0);
-}
-
-void printNum(double value)
-{
-    char buf[32];
-    snprintf(buf, sizeof(buf), "%g", value);
-    runtime_write(1, buf, (double)strlen(buf));
-}
-
-void printlnNum(double value)
-{
-    char buf[32];
-    snprintf(buf, sizeof(buf), "%g", value);
-    runtime_write(1, buf, (double)strlen(buf));
-    runtime_write(1, "\n", 1.0);
-}
-
-char *input(void)
+char *aloha_sys_input(void)
 {
     char *line = NULL;
     size_t len = 0;
-    ssize_t read = getline(&line, &len, stdin);
+    ssize_t nread = getline(&line, &len, stdin);
 
-    if (read == -1)
+    if (nread == -1)
     {
         free(line);
         return NULL;
     }
 
-    if (read > 0 && line[read - 1] == '\n')
+    if (nread > 0 && line[nread - 1] == '\n')
     {
-        line[read - 1] = '\0';
+        line[nread - 1] = '\0';
     }
 
     return line;
+}
+
+void print(const char *str)
+{
+    size_t len = strlen(str);
+    write(1, str, len);
+}
+
+void println(const char *str)
+{
+    size_t len = strlen(str);
+    write(1, str, len);
+    write(1, "\n", 1);
+}
+
+void printNum(double value)
+{
+    char buf[32];
+    int len = snprintf(buf, sizeof(buf), "%g", value);
+    if (len > 0)
+    {
+        write(1, buf, (size_t)len);
+    }
+}
+
+void printlnNum(double value)
+{
+    char buf[32];
+    int len = snprintf(buf, sizeof(buf), "%g", value);
+    if (len > 0)
+    {
+        write(1, buf, (size_t)len);
+    }
+    write(1, "\n", 1);
+}
+
+char *input(void)
+{
+    return aloha_sys_input();
 }
